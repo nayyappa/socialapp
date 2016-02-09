8ea7e362a8a2f383184ca81c0da7be6cfad0bf10
nickkevin <nickkevin35@gmail.com>
2016-02-09 23:15:23 +0530

    Added profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  #get 'users/show'
+
   devise_for :users, controllers: {registrations: 'registrations'}
   #get 'pages/home'
 
@@ -7,6 +9,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
     root 'pages#home'
+
+    resources :users, only: [:show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

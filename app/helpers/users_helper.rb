15a672f94ad7d4a358b93a0d5d25037bfc02d341
nickkevin <nickkevin35@gmail.com>
2016-02-11 22:58:16 +0530

    Added helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+	def action_buttons(user)
+		case current_user.friendship_status(user)
+		when "friends"
+			"Remove Friendship"
+		when "not_friends"
+			"Add as a friend"
+		when "pending"
+			"Cancel Friendship"
+		when "Requested"
+			"Accept or Deny"
+		end
+	end
 end

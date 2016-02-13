6182a6b1574625211fcbf284e19960e7a5d1ecd7
nickkevin <nickkevin35@gmail.com>
2016-02-13 15:09:21 +0530

    Added activity

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
+before_action :authenticate_user!, only:[:index]
 before_action :set_user, only:[:show]
 
   def index
-  	@users = User.all
+  	case params[:people]
+  	when "friends"
+  		@users = current_user.active_friends
+  	when "pending"
+  		@users = current_user.pending_friend_requests_to.map(&:user)
+  	when "requests"
+  		@users = current_user.pending_friend_requests_from.map(&:user)
+  	else
+  	@users = User.where.not(id: current_user.id)
+  end
   end
   
   def show
+    @activities = PublicActivity::Activity.where(owner_id: @user.id) 
   end
 
   private
